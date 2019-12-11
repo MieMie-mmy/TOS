c5ed14240963f9a107f74c77df2df017267e8d87
moonsan <capital.moonsan@gmail.com>
2019-12-11 17:28:20 +0630

    add inventory by moon san

---
+++ TOS/Controllers/InventoriesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TOS.Controllers
+{
+    public class InventoriesController : Controller
+    {
+        // GET: Inventories
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Inventories()
+        {
+            return View();
+        }
+    }
+}
