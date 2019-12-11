2ae6dccb5e09812b5878310a55367aea3a417d4d
cho-thae <chochothae.ucsy2017@gmail.com>
2019-12-11 16:32:25 +0630

    Registration add by cho thae

---
+++ TOS/Controllers/RegistrationsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TOS.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        // GET: Registrations
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
