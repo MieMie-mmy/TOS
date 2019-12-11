5a94ecc2cfdf443dc3bc5d2475b7d05ac217f360
myintyi <myintmyintyi1995@gmail.com>
2019-12-11 13:51:58 +0630

    by mie mie

---
+++ TOS/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TOS.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order_History
+        public ActionResult Order_History()
+        {
+            return View();
+        }
+    }
+}
