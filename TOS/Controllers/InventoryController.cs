89106067d48bb77ee28743399141551878cc6616
moonsan <capital.moonsan@gmail.com>
2019-12-11 15:32:00 +0630

    By moon san

---
+++ TOS/Controllers/InventoryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TOS.Controllers
+{
+    public class InventoryController : Controller
+    {
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Inventories()
+        {
+            return View();
+        }
+
+        public ActionResult Inventories_list()
+        {
+            return View();
+        }
+    }
+}
