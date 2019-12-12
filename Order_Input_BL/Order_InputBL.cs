7393bc380669da669e8a78a0364416dfcda5d231
myintyi <myintmyintyi1995@gmail.com>
2019-12-12 11:07:25 +0630

    by miemie

--- Order_Input_BL/Order_InputBL.cs
+++ Order_Input_BL/Order_InputBL.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using TOS_DL;
+using TOS_Model;
 
 namespace Order_Input_BL
 {
     public class Order_InputBL
     {
+        public DataTable JobTimeTable_Select(string CompanyCD)
+        {
+            TOSEntities ent = new TOSEntities();
+            var dtjobtimetable = ent.M_JobTimeable.Where(m => m.CompanyCD == CompanyCD).SingleOrDefault();
+            DataTable dt = new DataTable();
+            return  dt;
+        }
     }
 }
---
+++ TOS_Model/M_JobTimeableModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOS_Model
+{
+    public class M_JobTimeableModel: BaseModel
+    {
+        public string CompanyCD { get; set; }
+        public string OrderAblePeriod { get; set; }
+        public string CancelAblePeriod { get; set; }
+        public string ExceptionAboutPeriod { get; set; }
+    }
+}
