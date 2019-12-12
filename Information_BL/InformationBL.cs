e15455d59907cb74c78c79c7f04ab43417ee7fe2
AhMon <yinmonoo451@gmail.com>
2019-12-12 16:16:30 +0630

    amon

---
+++ Information_BL/InformationBL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using TOS_Model;
+using TOS_DL;
+
+
+namespace Information_BL
+{
+    public class InformationBL
+    {
+        public DataTable GetInformation(string CompanyCD)
+        {
+            BaseDL bdl = new BaseDL();
+            DataTable dtinfo = new DataTable();
+            SqlParameter[] prms = new SqlParameter[1];
+            prms[0] = new SqlParameter("@companyCD", SqlDbType.VarChar) { Value = CompanyCD};
+            dtinfo = bdl.SelectData("T_Information_Select", prms);
+            return dtinfo;
+        }
+     
+    }
+}
