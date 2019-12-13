39bc1f8481782d6aa89f839c796bedfbfa60bff9
myintyi <myintmyintyi1995@gmail.com>
2019-12-13 13:54:52 +0630

    miemie

--- TOS_Model/M_CompanyShippingModel.cs
+++ TOS_Model/M_CompanyShippingModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TOS_Model
 {
-    public class M_CompanyShipping:BaseModel
+    public class M_CompanyShippingModel:BaseModel
     {
         public string CompanyCD { get; set; }
         public int ShippingID { get; set; }
