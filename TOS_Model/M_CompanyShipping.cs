a90e080a6767675fd52f2e8b7e064cb4bcc1e0df
myintyi <myintmyintyi1995@gmail.com>
2019-12-13 13:53:40 +0630

    by miemie M_Companyshippingmodel

---
+++ TOS_Model/M_CompanyShipping.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOS_Model
+{
+    public class M_CompanyShipping:BaseModel
+    {
+        public string CompanyCD { get; set; }
+        public int ShippingID { get; set; }
+        public string ShippingName { get; set; }
+        public string ZipCD1 { get; set; }
+        public string ZipCD2 { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string TelephoneNO { get; set; }
+        public string FaxNO { get; set; }
+
+    }
+}
