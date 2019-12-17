e8245de21dfd7fc2449467e336319958f0391439
Ay Mi Soe <Ay Mi Soe@DESKTOP-DJ5ADD9>
2019-12-17 17:20:04 +0630

    aymi_new_bl

---
+++ Group_Entry_BL/Group_EntryBL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Entry_BL
+{
+    public class Group_EntryBL
+    {
+    }
+}
