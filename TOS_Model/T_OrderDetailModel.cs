3a191620b5331e9741c602b3e734b89d7097184f
Ay Mi Soe <Ay Mi Soe@DESKTOP-DJ5ADD9>
2019-12-17 10:18:43 +0630

    aymi

--- TOS_Model/T_OrderDetailModel.cs
+++ TOS_Model/T_OrderDetailModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TOS_Model
 {
- public class T_OrderDetailModel:BaseModel
+    public class T_OrderDetailModel : BaseModel
     {
         public string OrderID { get; set; }
 
@@ -14,12 +14,40 @@ namespace TOS_Model
 
         public int StockItem { get; set; }
 
-        public string SalePrice { get; set; }
+        public decimal SalePrice { get; set; }
 
-        public string TotalAmount { get; set; }
+        public decimal TotalAmount { get; set; }
 
-        public string OrderDateTime { get; set; }
+        public DateTime? OrderDateTime { get; set; }
 
-        public string ShippingDate { get; set; }
+        public DateTime? ShippingDate { get; set; }
+    }
+    public class T_OrderDetailModel2 : BaseModel
+    {
+        public int No { get; set; }
+
+        public string OrderID { get; set; }
+
+        public string MakerItemCD { get; set; }
+
+        public string ItemName { get; set; }
+
+        public string SizeName { get; set; }
+
+        public string ColorName { get; set; }
+
+        public string OrderItem { get; set; }
+
+        public string StockItem { get; set; }
+
+        public decimal SalePrice { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public DateTime? OrderDate { get; set; }
+
+        public DateTime? OrderTime { get; set; }
+
+        public DateTime? ShippingDate { get; set; }
     }
 }
