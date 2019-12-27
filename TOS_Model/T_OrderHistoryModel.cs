7dbe69bd3b0f0eafb034bfc3144547578fa5988e
khinsandarthein <thetyskhinsandar2019@gmail.com>
2019-12-27 17:51:42 +0630

    by khinsandar (change print colum and add delete Message)

--- TOS_Model/T_OrderHistoryModel.cs
+++ TOS_Model/T_OrderHistoryModel.cs
@@ -6,38 +6,7 @@ using System.Threading.Tasks;
 
 namespace TOS_Model
 {
-    //public class T_OrderHistoryModel
-    //{
-    //    public string CompanyCD { get; set; }
-    //    public string OrderID { get; set; }
-    //    public decimal? TotalAmount { get; set; }
-    //    public string DestinationName { get; set; }
-    //    public string OrderDateTime_h { get; set; }
-    //    public DateTime? AvailableShippingDate { get; set; }
-    //    public DateTime? OrderDateTime_d { get; set; }
-
-
-    //}
-    //public class T_OrderHistoryModel2
-    //{
-
-    //    public int? No { get; set; }
-    //    public string OrderID { get; set; }
-    //    public string MakerItemCD_gav { get; set; }
-    //    public string ItemName_gav { get; set; }
-    //    public string SizeName_gav { get; set; }
-    //    public string ColorName_gav { get; set; }
-    //    public int? OrderItem_gav { get; set; }
-    //    public int? StockItem_gav { get; set; }
-    //    public decimal? SalePrice_gav { get; set; }
-    //    public decimal? TotalAmount_gav { get; set; }
-    //    public string OrderDate_gav { get; set; }
-    //    public string OrderTime_gav { get; set; }
-    //    public DateTime? ShippingDate_gav { get; set; }
-    //    public string Memo_gav { get; set; }
-
-
-    //}
+    
     public class T_OrderHistorySearch
      {
         public string id { get; set; }
